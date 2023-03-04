96077e0e26a0a92c467d9e657fac39640c9ce3bb
mago_d_Mercurio <davila.feria@gmail.com>
2023-03-04 09:21:17 -0600

    add hobbies img

---
+++ validaciones.js
@@ -0,0 +1,57 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);  
+    }
+    
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = ""
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input)
+
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacio"
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+}
+
+const validadores = {
+    nacimiento: input => validarNacimiento(input),
+}
+
+const inputNacimiento = document.querySelector('#birth');
+
+inputNacimiento.addEventListener('blur', (evento) => {
+    validarNacimiento(evento.target);
+});
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+        if (input.validity[error]) {
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+
+    return mensaje
+}
+
